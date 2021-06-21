7c9cd40a277db164e73a99b22810198657bb0e6d
Devan Patel <Dev25@users.noreply.github.com>
2021-06-21 16:24:59 +0100

    fix: Remove deprecated `list()` function (#28)
    * Remove deprecated list() function
    
    * Update main.tf

--- main.tf
+++ main.tf
@@ -102,7 +102,7 @@ resource "google_dns_managed_zone" "public" {
   visibility  = "public"
 
   dynamic "dnssec_config" {
-    for_each = var.dnssec_config == {} ? [] : list(var.dnssec_config)
+    for_each = var.dnssec_config == {} ? [] : [var.dnssec_config]
     iterator = config
     content {
       kind          = lookup(config.value, "kind", "dns#managedZoneDnsSecConfig")
