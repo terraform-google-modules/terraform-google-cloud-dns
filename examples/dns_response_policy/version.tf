9a571cddf1955cb8a4a7a6484d9d0c569c084391
Andrew Peabody <andrewpeabody@google.com>
2023-10-09 09:08:25 -0700

    Update version.tf

--- examples/dns_response_policy/version.tf
+++ examples/dns_response_policy/version.tf
@@ -19,11 +19,9 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.0"
     }
   }
 }
