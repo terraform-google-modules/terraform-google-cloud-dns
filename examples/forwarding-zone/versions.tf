014dc1dc15b37494db286fb1ef12f971faa79bfc
Andrew Peabody <andrewpeabody@google.com>
2023-10-09 09:17:56 -0700

    Update versions.tf

--- examples/forwarding-zone/versions.tf
+++ examples/forwarding-zone/versions.tf
@@ -18,10 +18,10 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     google = {
-      source  = "hashicorp/google"
+      source = "hashicorp/google"
     }
     google-beta = {
-      source  = "hashicorp/google-beta"
+      source = "hashicorp/google-beta"
     }
   }
 }
