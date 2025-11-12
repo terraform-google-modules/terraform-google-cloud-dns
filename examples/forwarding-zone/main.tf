0ed181e67e5cdc43d05e7a24cfe26077d246f409
Saranya3 <ssranya@google.com>
2025-11-12 04:06:03 +0000

    feat: Updating Cloud DNS - Managed Zone, Response Policy terraform and adding tests along with metadata.yaml and metadata.display.yaml files

--- examples/forwarding-zone/main.tf
+++ examples/forwarding-zone/main.tf
@@ -16,7 +16,7 @@
 
 module "dns-forwarding-zone" {
   source  = "terraform-google-modules/cloud-dns/google"
-  version = "~> 6.0"
+  version = "~> 6.1.0"
 
   project_id = var.project_id
   type       = "forwarding"
@@ -35,4 +35,10 @@ module "dns-forwarding-zone" {
       forwarding_path = "default"
     }
   ]
+
+  iam_choice   = "iam_member"
+  managed_zone = "zone1"
+  role         = "roles/viewer"
+  member       = "serviceAccount:ci-account@${var.project_id}.iam.gserviceaccount.com"
+  members      = []
 }
